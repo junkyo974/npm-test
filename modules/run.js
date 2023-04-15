017930941706f595c0eed7bdea76d25370ce555b
junkyo <124774799+junkyo974@users.noreply.github.com>
2023-04-15 15:37:58 +0900

    module 연습

---
+++ modules/run.js
@@ -0,0 +1,17 @@
+// -------1---------------
+// const add = require("./math.js");
+
+// console.log(add(10,30));
+
+
+// -------2---------------
+// const {add} = require("./math.js");
+
+// console.log(add(10,30));
+
+
+
+// -------3---------------
+const {add} = require("./math.js");
+
+console.log(add(10,30));
