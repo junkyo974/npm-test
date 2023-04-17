9e891c23e4543e91ae782ca2084db996cf1e30c4
junkyo <124774799+junkyo974@users.noreply.github.com>
2023-04-17 19:06:52 +0900

    입문주차 실습

--- app.js
+++ app.js
@@ -3,11 +3,36 @@ const app = express();
 const port = 3000;
 const goodsRouter = require('./routes/goods.js');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.json());
 
-app.use("/api", goodsRouter);
+app.post("/", (req,res) => {
+  console.log(req.body);
+
+  res.send("기본 URI에 PSOT 메소드가 정상적으로 실행되었습니다.");
+})
+
+app.get('/', (req,res) => {
+  console.log(req.query);
+
+  const obj = {
+    "keykey" : "value 입니다.",
+    "이름입니다." : "이름일까요?"
+  }
+  
+  res.json(obj);
+})
+
+app.get("/:id", (req,res) => {
+  console.log(req.params);
+
+  res.send(":id URI에 정상적으로 반환되었습니다.");
+})
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+// app.use("/api", goodsRouter);
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
--- routes/goods.js
+++ routes/goods.js
@@ -1,12 +1,59 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send("default url goods.js GET Method");;
-})
+// /routes/goods.js
+const goods = [
+    {
+      goodsId: 4,
+      name: "상품 4",
+      thumbnailUrl:
+        "https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg",
+      category: "drink",
+      price: 0.1,
+    },
+    {
+      goodsId: 3,
+      name: "상품 3",
+      thumbnailUrl:
+        "https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg",
+      category: "drink",
+      price: 2.2,
+    },
+    {
+      goodsId: 2,
+      name: "상품 2",
+      thumbnailUrl:
+        "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
+      category: "drink",
+      price: 0.11,
+    },
+    {
+      goodsId: 1,
+      name: "상품 1",
+      thumbnailUrl:
+        "https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg",
+      category: "drink",
+      price: 6.2,
+    },
+  ];
 
-router.get('/about', (req, res) => {
-    res.send("goods.js about PATH");
-})
+  router.get("/goods", (req, res) => {
+    res.status(200).json({goods})
+  });
+
+  router.get("/goods/:goodsId", (req, res)=>{
+    const { goodsId } = req.params;
+    
+    let result = null;
+    for(const goods of goods){
+        if(Number(goodsId) === goods.goodsId){
+            result = good;
+        }
+    }
+    // = 동일한 코드
+    // const [result] goods.filter((good) =>Number(goodsId) === good.goodsId)
+
+    res.status(200).json({detail: result});
+  })
 
 module.exports = router;
